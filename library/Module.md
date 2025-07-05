d3065294168461ba1a3255eaf56033d391e4112d
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2025-07-05 19:41:03 +0200

    Fixing structure

---
+++ library/Module.md
@@ -0,0 +1,4 @@
+# Module angelos-project-selector
+# Package org.angproj.io.sel
+
+<img src="https://angelos-project.com/images/angelos.png" alt="Angelos Project™" style="width:120px; height:auto; margin-right:1em; margin-bottom:1em;" align="left">
---
+++ library/build.gradle.kts
@@ -0,0 +1,155 @@
+import com.vanniktech.maven.publish.SonatypeHost
+import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
+import java.net.URL
+
+object This {
+    const val longName = "Concurrent selector - Angelos Project™"
+    const val longDescription = "Concurrency utilities purposefully built to give a hassle-free experience in developing concurrent programming built on top of kotlinx.coroutines without exposing the programmer to any setup of underlying technology."
+    const val url = "https://github.com/angelos-project/angelos-project-selector"
+}
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.vanniktech.mavenPublish)
+    alias(libs.plugins.dokka)
+    alias(libs.plugins.kover)
+}
+
+group = "org.angproj.io.sel"
+version = "0.1.0"
+
+kotlin {
+    explicitApi()
+    jvmToolchain(libs.versions.jvm.toolchain.get().toInt())
+
+    jvm()
+    /*js {
+        browser()
+        nodejs()
+    }
+    // WASM and similar
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs {
+        browser()
+        nodejs()
+    }
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmWasi { nodejs() }*/
+    // Android
+    androidTarget {
+        publishLibraryVariants("release")
+    }
+    androidNativeArm32()
+    androidNativeArm64()
+    androidNativeX64()
+    androidNativeX86()
+    // Linux
+    linuxArm64()
+    linuxX64()
+    // macOS
+    macosArm64()
+    macosX64()
+    // MingW
+    mingwX64()
+    // iOS
+    iosArm64()
+    iosX64()
+    iosSimulatorArm64()
+    // tvOS
+    tvosArm64()
+    tvosX64()
+    tvosSimulatorArm64()
+    // watchOS
+    watchosArm32()
+    watchosArm64()
+    watchosDeviceArm64()
+    watchosSimulatorArm64()
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.kotlin.coroutines.core)
+        }
+        commonTest.dependencies {
+            implementation(libs.kotlin.test)
+            implementation(libs.kotlin.coroutines.test)
+        }
+        androidMain.dependencies {
+            implementation(libs.kotlin.coroutines.android)
+        }
+    }
+}
+
+android {
+    namespace = group.toString()
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+    /*compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }*/
+}
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    //signAllPublications()
+
+    /**
+     * The temporary artifact setup, final is coming later at some point.
+     * DO NOT USE FOR SONATYPE NEXUS
+     * */
+    coordinates(group.toString(), rootProject.name, version.toString())
+
+    pom {
+        name.set(This.longName)
+        description.set(This.longDescription)
+        inceptionYear.set("2025")
+        url.set(This.url)
+
+        licenses {
+            license {
+                name.set("MIT License")
+                url.set("https://opensource.org/licenses/MIT")
+                distribution.set("repo")
+            }
+        }
+        developers {
+            developer {
+                name.set("Kristoffer Paulsson")
+                email.set("kristoffer.paulsson@talenten.se")
+                url.set("https://github.com/kristoffer-paulsson")
+            }
+        }
+        scm {
+            url.set(This.url)
+            connection.set("scm:git:git://github.com/angelos-project/angelos-project-selector.git")
+            developerConnection.set("scm:git:ssh://github.com:angelos-project/angelos-project-selector.git")
+        }
+    }
+}
+
+tasks.dokkaHtml {
+    dokkaSourceSets {
+        named("commonMain"){
+            moduleName.set(This.longName)
+            includes.from("Module.md")
+            sourceLink {
+                localDirectory.set(file("src/commonMain/kotlin"))
+                remoteUrl.set(URL(This.url + "/tree/master/src/commonMain/kotlin"))
+                remoteLineSuffix.set("#L")
+            }
+        }
+    }
+}
+
+kover {
+    reports {
+        total {
+            xml.onCheck.set(true)
+            html.onCheck.set(true)
+        }
+    }
+}
